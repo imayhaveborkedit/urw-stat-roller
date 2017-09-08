0d30aeb2a92643e93f874bd5df8f7035ba4624df
Daniel <dalmodovar.1337@gmail.com>
2017-09-08 00:22:10 -0400

    Improve error logging format
    For what its worth I guess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import traceback
 
 from threading import Lock
@@ -13,8 +14,9 @@ ui = Ui(h)
 def exhook(*args):
     with l:
         with open("error.txt", 'a') as f:
+            f.write(f"{'#' * 20} {time.asctime()} {'#' * 20}\n")
             f.writelines(traceback.format_exception(*args))
-            f.write('\n')
+            f.write('\n\n')
 
         ui.on_error(*args)
 
